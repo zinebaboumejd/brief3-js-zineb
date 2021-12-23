57769040fd9c55340092b4d7f4c0010c4ca1faf9
zineb ABOUMEJD <zinebaboumejd@gmail.com>
2021-12-23 16:44:35 +0100

    first commit

---
+++ agencedelocation.js
@@ -0,0 +1,94 @@
+ul.navbar-nav li.nav-item a.nav-link, ul.navbar-nav li.nav-item.active a.nav-link {
+    color: #051922;
+  }
+  
+  .top-header-area {
+    position: absolute;
+    z-index: 999;
+    width: 100%;
+    padding: 25px 0;
+  }
+  
+  .top-header-area.white ul.navbar-nav li.nav-item a.nav-link, .top-header-area.white ul.navbar-nav li.nav-item.active a.nav-link {
+    color: #fff;
+  }
+  
+  ul.navbar-nav li.nav-item a.nav-link, ul.navbar-nav li.nav-item.active a.nav-link {
+    color: #fff;
+    font-weight: 700;
+    margin-right: 14px;
+  }
+  
+  a.navbar-brand img {
+    max-width: 150px;
+  }
+  
+  nav.main-menu ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  nav.main-menu ul li {
+    display: inline-block;
+  }
+  
+  nav.main-menu ul li a {
+    color: #fff;
+    font-weight: 700;
+    display: block;
+    padding: 15px;
+  }
+  
+  nav.main-menu ul > li {
+    position: relative;
+  }
+  
+  nav.main-menu ul ul.sub-menu {
+    position: absolute;
+    background-color: #fff;
+    width: 220px;
+    padding: 15px 15px;
+    margin: 0;
+    left: 0;
+    top: 50px;
+    border-radius: 3px;
+    -webkit-transition: 0.3s;
+    -o-transition: 0.3s;
+    transition: 0.3s;
+    opacity: 0;
+    visibility: hidden;
+    -webkit-box-shadow: 0 0 20px #555555;
+    box-shadow: 0 0 20px #555555;
+  }
+  
+  nav.main-menu ul ul.sub-menu li {
+    display: block;
+    text-align: left;
+  }
+  
+  nav.main-menu ul ul.sub-menu li a {
+    color: #555;
+    font-weight: 600;
+    padding: 7px 10px;
+    font-size: 13px;
+  }
+  
+  nav.main-menu ul > li:hover ul {
+    opacity: 1;
+    visibility: visible;
+  }
+  
+  nav.main-menu ul li:last-child a {
+    display: inline-block;
+  }
+  
+  nav.main-menu > ul li:last-child {
+    float: right;
+  }
+  
+  .site-logo {
+    float: left;
+    max-width: 150px;
+  }
+  
